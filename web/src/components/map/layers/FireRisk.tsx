802c4a2229f87daa41cae5b7f354fa6aeff0ff4d
novmdecore <2015200799@alu.ruc.edu>
2025-09-01 22:04:16 -0700

    change firerisk map

--- web/src/components/map/layers/FireRisk.tsx
+++ web/src/components/map/layers/FireRisk.tsx
@@ -4,35 +4,36 @@ import { Source, Layer } from 'react-map-gl/mapbox'
 type Props = {
   opacity?: number
   visible?: boolean
-  /** 把本图层插到某个已存在图层之前，控制层级；不传则在最上面 */
-  beforeId?: string
+  beforeId?: string // 可选：想插在已有图层之前
 }
 
-const SRC_ID = 'risk-psta'
-const LAYER_ID = 'risk-psta'
+const SRC_ID = 'cwfis-fdr'
+const LAYER_ID = 'cwfis-fdr'
 
-// 省府 PSTA（ArcGIS export）栅格切片（bbox 模板由 mapbox-gl 负责填充）
-const BC_PSTA_EXPORT =
-  'https://delivery.maps.gov.bc.ca/arcgis/rest/services/whse/bcgw_pub_whse_land_and_natural_resource/MapServer/export' +
-  '?f=image&format=png32&transparent=true' +
-  '&bbox={bbox-epsg-3857}&bboxSR=3857&imageSR=3857&size=256,256' +
-  '&layers=show:17' // Fire Threat Rating
+// CWFIS WMS：当前火险等级（Fire Danger Rating）
+const CWFIS_FDR_WMS =
+  'https://cwfis.cfs.nrcan.gc.ca/geoserver/public/wms?' +
+  'service=WMS&version=1.1.1&request=GetMap' +
+  '&layers=public:fdr_current' +     // ← 关键：正确图层名（可换成其他）
+  '&styles=' +                        // WMS 允许空 styles
+  '&format=image/png&transparent=true' +
+  '&srs=EPSG:3857&bbox={bbox-epsg-3857}' +  // Mapbox 使用 3857
+  '&width=256&height=256'
 
-export default function FireRisk({ opacity = 0.3, visible = true, beforeId }: Props) {
+export default function FireRisk({ opacity = 0.6, visible = true, beforeId }: Props) {
   return (
     <Source
       id={SRC_ID}
       type="raster"
-      tiles={[BC_PSTA_EXPORT]}
+      tiles={[CWFIS_FDR_WMS]}
       tileSize={256}
-      attribution="PSTA © Province of BC"
+      attribution="CWFIS © Natural Resources Canada"
     >
       <Layer
         id={LAYER_ID}
         type="raster"
         paint={{ 'raster-opacity': opacity }}
         layout={{ visibility: visible ? 'visible' : 'none' }}
-        // 控制插入顺序（可选）
         beforeId={beforeId}
       />
     </Source>
