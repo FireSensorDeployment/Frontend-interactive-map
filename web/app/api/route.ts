51f3b81356bf6077cb6ff2f3564dd8200704c97f
Jeff Ma <jeff.ma713@gmail.com>
2025-09-15 13:34:16 -0700

    Modify route.ts to use mock response. Modify .gitignore to include more file types

--- web/app/api/route.ts
+++ web/app/api/route.ts
@@ -1,5 +1,9 @@
+import { randomPoint, area, bbox } from "@turf/turf";
+import { NextResponse } from "next/server"; 
 export async function POST(req: Request) {
     const body = await req.json();
+    /* 
+    # 暂时Comment直到后期串联起来
     const base = process.env.PY_BACKEND_URL!;
     const r = await fetch(`${base}/api/sensors/plan`, {
       method: 'POST',
@@ -9,5 +13,20 @@ export async function POST(req: Request) {
     const text = await r.text();
     const ct = r.headers.get('content-type') ?? 'application/json';
     return new Response(text, { status: r.status, headers: { 'content-type': ct } });
+    */
+
+    // Mocking backend response
+    if (!body || !body.geometry || area(body) < 10000 ) {
+      return new Response(
+        JSON.stringify({ message: "Area of interest is too small or invalid."}), { status: 400, headers: {'Content-Type' : 'application/json'}}
+      );
+    }
+
+    const numberOfPoints = 5;
+    // 这里有建议用mask, 但是目前版本没有这个功能, 是否要升级?
+    const points = randomPoint(numberOfPoints, { bbox : bbox(body) }); 
+
+    return NextResponse.json(points)
+
   }
   
